975587c39b86933540d6ad37a82da169a106732e
NeiltonSeguins <seguins.neilton@gmail.com>
2021-12-08 19:05:56 -0300

    Faz ajustes na responsividade e inicia a criação da seção de fotos da página. Adiciona estilos

--- photolead/src/components/Main.jsx
+++ photolead/src/components/Main.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import Hero from "../components/Hero";
+import '../styles/Css/main.css'
 
 const Main = () => {
   return (
     <>
-      <main>
+      <main className="main">
         <Hero />
+        <nav className="main_nav">
+          <ul className="main_ul">
+            <li>Início</li>
+            <div className="main_separator"></div>
+            <li>Explorar</li>
+          </ul>
+          <div className="main_span"></div>
+        </nav>
+        <div className="main_photos"></div>
       </main>
     </>
   );
